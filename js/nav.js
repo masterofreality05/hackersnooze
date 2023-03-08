97e85cce8be9921aea52c0d37047b6cd0e3fe139
SEAN WHITTY <SEANWHITTY92@gmail.com>
2023-03-08 16:39:07 +0100

    need to finalize the delete button

---
+++ js/nav.js
@@ -0,0 +1,108 @@
+"use strict";
+
+/******************************************************************************
+ * Handling navbar clicks and updating navbar
+ */
+
+/** Show main list of all stories when click site name */
+
+function navAllStories(evt) {
+  console.debug("navAllStories", evt);
+  hidePageComponents();
+  putStoriesOnPage();
+}
+
+$body.on("click", "#nav-all", navAllStories);
+
+/** Show login/signup on click on "login" */
+
+function navLoginClick(evt) {
+  console.debug("navLoginClick", evt);
+  hidePageComponents();
+  $loginForm.show();
+  $signupForm.show();
+}
+
+$navLogin.on("click", navLoginClick);
+
+/** When a user first logins in, update the navbar to reflect that. */
+
+function updateNavOnLogin() {
+  console.debug("updateNavOnLogin");
+  $(".main-nav-links").show();
+  $navLogin.hide();
+  $navLogOut.show();
+  $navUserProfile.text(`${currentUser.username}`).show();
+  $('.navbar-brand').append('<span><a id="submitShow" class="userLink">Submit </a></span>')
+  $('.navbar-brand').append('<span><a id="favorites" class="userLink">Favorites</a></span')
+  $('.navbar-brand').append('<a id="myStories" class="userLink">My Stories</a>')
+  $('.navbar-brand').append($newStoryForm)
+  $newStoryForm.hide()
+
+  $('#submitShow').on("click", function(e){
+    
+    e.preventDefault();
+    console.log("hey it clicked")
+    $newStoryForm.show()
+   })
+
+   function putFavStoriesOnPage() {
+   
+  }
+  
+   $('#favorites').on("click", async function(e){
+    let storyObjectList = []
+    $allStoriesList.empty()
+     e.preventDefault()
+     console.log("clicked favourites")
+     for(let faves of currentUser.favorites){
+      const response = await axios({
+        url: `${BASE_URL}/stories/${faves}`,
+        method: "GET",
+        
+      });
+      console.log("this is our response " +  response.data.story)
+      let respondedStory = response.data.story;
+      
+     
+      storyObjectList.push(respondedStory)
+      
+    }
+    console.log("storyObjectlist" + storyObjectList)
+  
+      const stories = storyObjectList.map(story => new Story(story))
+      for(let story of stories){
+        let markedUpFav =generateStoryMarkup(story)
+        $allStoriesList.append(markedUpFav).append(`<a id="${story.storyId}"class="remove story-user">Remove from favourites</a>`)
+        let removeButtons = document.getElementsByClassName('remove')
+
+        for(let button of removeButtons){
+          button.addEventListener("click", function(e){
+            let storyId = e.target.getAttribute("id")
+            console.log("story id is" + storyId)
+            let index = currentUser.favorites.indexOf(storyId);
+            console.log("index is " + index)
+            currentUser.favorites.splice(index, 1).then($allStoriesList)
+          })
+        
+        }
+
+      }
+     
+
+
+
+     
+    
+   })}
+
+ 
+
+
+
+
+
+
+
+
+
---
+++ js/stories.js
@@ -0,0 +1,58 @@
+"use strict";
+
+// This is the global list of the stories, an instance of StoryList
+let storyList;
+
+/** Get and show stories when site first loads. */
+
+async function getAndShowStoriesOnStart() {
+  storyList = await StoryList.getStories();
+  $storiesLoadingMsg.remove();
+
+  putStoriesOnPage();
+}
+
+function generateStoryMarkup(story) {
+  console.debug("generateStoryMarkup", story);
+
+  const hostName = story.getHostName();
+  let $favorite = $(`<a class="favorite" id="${story.storyId}">Add to Favorites</a>`)
+  $favorite.on("click", function(e){
+    currentUser.favorites.push($favorite.attr("id"))
+    //successfully pushes the story ID of favourited story to the currentUser.
+  })
+/console.log("hostname is " + hostName)
+console.log("properties passed are " + story)
+
+  return $(`
+      <li id="${story.storyId}">
+        <a href="${story.url}" target="a_blank" class="story-link">
+          ${story.title}
+        </a>
+        <small class="story-hostname">(${hostName})</small>
+        <small class="story-author">by ${story.author}</small>
+        <small class="story-user">posted by ${story.username}</small>
+      </li>
+    
+    `).append($favorite);
+}
+
+/** Gets list of stories from server, generates their HTML, and puts on page. */
+
+function putStoriesOnPage() {
+  console.debug("putStoriesOnPage");
+
+  $allStoriesList.empty();
+
+  // loop through all of our stories and generate HTML for them
+  for (let story of storyList.stories) {
+    const $story = generateStoryMarkup(story);
+    $allStoriesList.append($story);
+  }
+
+  $allStoriesList.show();
+}
+
+function getFaves(){
+}
+
